44d4bfba9fc8bd15c3c25abeaeaa0d21ab3b8f41
Aras <dev.arassanivar@gmail.com>
2020-06-06 16:01:39 +0300

    fix

--- src/Controllers/Auth/LoginController.php
+++ src/Controllers/Auth/LoginController.php
@@ -36,7 +36,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('auth.login');
+        return view('cms::auth.login');
     }
 
     /**
