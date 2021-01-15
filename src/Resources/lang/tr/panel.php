16de2895abef96593e2dd6aa1542e074a64a5b3a
Aras <dev.arassanivar@gmail.com>
2021-01-16 02:09:04 +0300

    login çeviri tamamlandı

---
+++ src/Resources/lang/tr/panel.php
@@ -0,0 +1,18 @@
+<?php
+
+    return [
+
+        'login' => 'Giriş Yapın',
+        'logout' => 'Çıkış Yapın',
+        'email' => 'E-Posta',
+        'password' => 'Şifre',
+        'remember_me' => 'Beni Hatırla',
+        'parent' => 'Bağlı olduğu sayfa',
+        'prime_login' => 'Prime\'a devam edebilmek için lütfen giriş yapın.',
+        'lost_password' => 'Şifrenizi mi unuttunuz?',
+
+
+    ];
+
+
+?>
