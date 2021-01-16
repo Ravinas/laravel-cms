db7904223aa5a803870243d656600dbb013bb8b9
Aras <dev.arassanivar@gmail.com>
2021-01-16 04:41:57 +0300

    iskelet yenilendi

---
+++ src/Resources/views/panel/newinc/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-left">
+            <p>2020 &copy; Prime</p>
+        </div>
+        <div class="float-right">
+            <p>ARME Yazılım Donanım A.Ş</p>
+        </div>
+    </div>
+</footer>
