b8a07653f9da00ff2b2f07b56a507b114ac64ebc
Aras <dev.arassanivar@gmail.com>
2021-01-23 10:45:00 +0300

    menu tamamlandı

--- src/Resources/views/panel/menu/item-children.blade.php
+++ src/Resources/views/panel/menu/item-children.blade.php
@@ -1,14 +1,14 @@
 <ol>
-    @foreach ($childs as $children)
-    <li class="mt-1" id="menuItem-{{ $children->id}}" style="list-style: none;">
-        <div class="list-group-item d-flex justify-content-end align-items-center">
-          <span class="mr-auto p-2">{!!  $children->text !!}</span>
-          <a href="{!! route('delete-item',['menuitem' => $item->id]) !!}" class="btn icon btn-info ml-2" data-id="{!! $item->id !!}"><i data-feather="plus-circle"></i></a>
-          <a href="#" class="btn icon btn-danger ml-2 del" data="{{ $menu->id }}" data-url="{!! route('delete-item',['menuitem' => $children->id]) !!}"><i data-feather="delete"></i></a>
-        </div>
-        @if($children->children)
-           @include('cms::panel.menu.new-item-children',['childs' => $children->children])
-        @endif
-    </li>
-    @endforeach
+  @foreach ($childs as $children)
+  <li class="mt-1" id="menuItem-{{ $children->id}}" style="list-style: none;">
+      <div class="list-group-item d-flex justify-content-end align-items-center">
+        <span class="mr-auto p-2">{!! Str::ucfirst($children->text)  !!}</span>
+        <a href="{!! route('delete-item',['menuitem' => $item->id]) !!}" class="btn icon btn-info ml-2" data-id="{!! $item->id !!}"><i data-feather="plus-circle"></i></a>
+        <a href="#" class="btn icon btn-danger ml-2 del" data="{{ $menu->id }}" data-url="{!! route('delete-item',['menuitem' => $children->id]) !!}"><i data-feather="delete"></i></a>
+      </div>
+      @if($children->children)
+         @include('cms::panel.menu.new-item-children',['childs' => $children->children])
+      @endif
+  </li>
+  @endforeach
 </ol>
--- src/Resources/views/panel/menu/items.blade.php
+++ src/Resources/views/panel/menu/items.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div class="main-content container-fluid">
     <div class="page-title">
-        <h3>Ana Menü</h3>
-        <p class="text-subtitle text-muted">Bu kısımda Ana Menü'nün elemanlarını ekleyebilir , silebilir ve sıraya sokabilirsiniz</p>
+        <h3>{!! Str::ucfirst($menu->text)  !!}</h3>
+        <p class="text-subtitle text-muted">{{ trans('cms::panel.menu_items_info') }}</p>
     </div>
     <section class="section">
         <div class="row mb-4">
@@ -11,7 +11,7 @@
                 <div class="card">
                     <div class="card-header">
                         <h3 class='card-heading p-1 pl-3 float-left'>{!! trans('cms::panel.items') !!}</h3>
-                        <button type="button" class="btn icon icon-left btn-primary float-right" data-toggle="modal" data-target="#inlineForm"><i data-feather="edit" ></i> Eleman Ekleyin</button>
+                        <button type="button" class="btn icon icon-left btn-primary float-right" data-toggle="modal" data-target="#inlineForm"><i data-feather="edit" ></i>{{ trans('cms::panel.add') }}</button>
                         <div class="form-group">
                             <!--Modal -->
                             <div class="modal fade text-left" id="inlineForm" tabindex="-1" role="dialog"
@@ -19,7 +19,7 @@
                                 <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                    <h4 class="modal-title" id="myModalLabel33">Menü Elemanı Oluştur </h4>
+                                    <h4 class="modal-title" id="myModalLabel33">{{ trans('panel.crate_menu_item') }} </h4>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <i data-feather="x"></i>
                                     </button>
@@ -27,40 +27,40 @@
                                     <form id="menuform">
                                     <input type="hidden" name="menu_id" value="{!! $menu->id !!}">
                                     <div class="modal-body">
-                                        <label>Metin </label>
+                                        <label>{{ trans('cms::panel.text') }}</label>
                                         <div class="form-group">
-                                            <input type="text" placeholder="Menüde Görünmesini İstediğiniz Metin (Örn: Hakkımızda)" class="form-control" name="text" required>
+                                            <input type="text" placeholder="{{ trans('panel.text') }}" autocomplete="none" class="form-control" name="text" required>
                                         </div>
-                                        <label>Tip</label>
+                                        <label>{{ trans('cms::panel.type') }}</label>
                                         <div class="form-group">
                                             <select class="form-select" id="basicSelect" name="type">
-                                                <option value="1">Single</option>
-                                                <option value="2">Dropdown</option>
+                                                <option value="1">{{ trans('cms::panel.single') }}</option>
+                                                <option value="2">{{ trans('cms::panel.dropdwon') }}</option>
                                             </select>
                                         </div>
-                                        <label>Bağlantı Tipi</label>
+                                        <label>{{ trans('cms::panel.url_type') }}</label>
                                         <div class="form-group">
                                             <select class="form-select" id="basicSelect" name="link_type">
-                                                <option value="0">Internal</option>
-                                                <option value="1">External</option>
+                                                <option value="0">{{ trans('cms::panel.internal') }}</option>
+                                                <option value="1">{{ trans('cms::panel.external') }}</option>
                                             </select>
                                         </div>
                                         <label>Tip</label>
                                         <div class="form-group">
-                                            <select class="form-select" id="basicSelect" name="type">
+                                            <select class="form-select" id="basicSelect" name="link">
                                                 @foreach($urls as $url)
                                                     <option value="{!! $url->url !!}">{!!  $url->url !!}</option>
                                                 @endforeach
                                             </select>
                                         </div>
-                                        <label>İkon (İsteğe Bağlı)</label>
+                                        <label>{{ trans('panel.icon') }}</label>
                                             <div class="input-group mb-3">
                                                 <span class="input-group-text" id="inputGroupFileAddon01"><i data-feather="upload"></i></span>
                                                 <div class="form-file">
                                                     <input type="file" class="form-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
                                                     <label class="form-file-label" for="inputGroupFile01">
-                                                        <span class="form-file-text">İkon seçin..</span>
-                                                        <span class="form-file-button">Yükle</span>
+                                                        <span class="form-file-text">{{ trans('cms::panel.choose_icon') }}</span>
+                                                        <span class="form-file-button">{{ trans('cms::panel.upload') }}</span>
                                                     </label>
                                                 </div>
                                             </div>
@@ -68,7 +68,69 @@
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-primary ml-1" data-dismiss="modal">
                                         <i class="bx bx-check d-block d-sm-none"></i>
-                                        <span class="d-none d-sm-block" id="post">Oluştur</span>
+                                        <span class="d-none d-sm-block" id="post">{{ trans('cms::panel.create') }}</span>
+                                        </button>
+                                    </div>
+                                    </form>
+                                </div>
+                                </div>
+                            </div>
+                            <!--Edit Modal -->
+                            <div class="modal fade text-left" id="editinlineForm" tabindex="-1" role="dialog"
+                                aria-labelledby="myModalLabel33" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h4 class="modal-title" id="myModalLabel33">{{ trans('cms::panel.edit_menu_item') }}</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <i data-feather="x"></i>
+                                    </button>
+                                    </div>
+                                    <form id="editform">
+                                    <input type="hidden" id="edit_item_id" name="item_id" value="{!! $menu->id !!}">
+                                    <div class="modal-body">
+                                        <label>Metin </label>
+                                        <div class="form-group">
+                                            <input type="text" placeholder="Menüde Görünmesini İstediğiniz Metin (Örn: Hakkımızda)" class="form-control" name="edit_text" autocomplete="none" required id="edit_text">
+                                        </div>
+                                        <label>Tip</label>
+                                        <div class="form-group">
+                                            <select class="form-select" id="edit_type" name="edit_type">
+                                                <option value="1">{{ trans('cms::panel.single') }}</option>
+                                                <option value="2">{{ trans('cms::panel.dropdwon') }}</option>
+                                            </select>
+                                        </div>
+                                        <label>Bağlantı Tipi</label>
+                                        <div class="form-group">
+                                            <select class="form-select" id="edit_link_type" name="edit_link_type">
+                                                <option value="0">{{ trans('cms::panel.internal') }}</option>
+                                                <option value="1">{{ trans('cms::panel.external') }}</option>
+                                            </select>
+                                        </div>
+                                        <label>Bağlantı</label>
+                                        <div class="form-group">
+                                            <select class="form-select" id="edit_link" name="edit_link">
+                                                @foreach($urls as $url)
+                                                    <option value="{!! $url->url !!}">{!!  $url->url !!}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <label>{{ trans('panel.icon') }}</label>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text" id="inputGroupFileAddon01"><i data-feather="upload"></i></span>
+                                                <div class="form-file">
+                                                    <input type="file" class="form-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                                                    <label class="form-file-label" for="inputGroupFile01">
+                                                        <span class="form-file-text">{{ trans('cms::panel.choose_icon') }}</span>
+                                                        <span class="form-file-button">{{ trans('cms::panel.upload') }}</span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-primary ml-1" data-dismiss="modal">
+                                        <i class="bx bx-check d-block d-sm-none"></i>
+                                        <span class="d-none d-sm-block" id="edit_post">{{ trans('cms::panel.create') }}</span>
                                         </button>
                                     </div>
                                     </form>
@@ -81,11 +143,12 @@
                         <div class="row">
                             <ol class="sortable" style="list-style: none;">
                                 @foreach ($menu_items as $item)
+
                                 <li class="mt-1" id="menuItem-{{$item->id}}" style="list-style: none;">
-                                    <div class="list-group-item d-flex justify-content-end align-items-center">
-                                      <span class="mr-auto p-2">{!! $item->text !!}</span>
-                                      <a href="" class="btn icon btn-info ml-2" data-id="{!! $item->id !!}"><i data-feather="plus-circle"></i></a>
-                                      <a href="{!! route('delete-item',['menuitem' => $item->id]) !!}" class="btn icon btn-danger ml-2 del" data-id="{!! $item->id !!}"><i data-feather="delete"></i></a>
+                                    <div class="list-group-item d-flex justify-content-end align-items-center" style="{{ $item->parent_id == 0 ? 'border-color:#26c6da;color:black;' : 'background-color:#D1F8F8;' }}">
+                                      <span class="mr-auto p-2">{!! Str::ucfirst($item->text)  !!}</span>
+                                      <a href="#" class="btn icon btn-info ml-2 edit_item" data-id="{!! $item->id !!}"><i data-feather="plus-circle"></i></a>
+                                      <a href="{!! route('delete-item',['menuitem' => $item->id]) !!}" class="btn icon btn-danger ml-2 del" data-id="{!! $item->id !!}" ><i data-feather="delete"></i></a>
                                     </div>
                                     @if($item->children)
                                         @include('cms::panel.menu.new-item-children',['childs' => $item->children])
@@ -126,7 +189,9 @@
             });
         }
 
-        $('#post').click(function (e) {
+
+
+        $('#edit_post').click(function (e) {
          e.preventDefault();
          var csrf = "{!! csrf_token() !!}";
          $.ajaxSetup({
@@ -136,6 +201,29 @@
         });
 
          $.ajax({
+             url:'{!! route('edit-menuitem-ajax') !!}',
+             method:'post',
+             data:$('#editform').serialize(),
+             success:function (response) {
+                 if (response.success == "Ok")
+                 {
+                    $('#editinlineForm').modal('hide');
+                    window.location.reload(true);
+                 }
+             }
+         });
+        });
+
+
+        $('#post').click(function (e) {
+         e.preventDefault();
+         var csrf = "{!! csrf_token() !!}";
+         $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': csrf
+            }
+        });
+         $.ajax({
              url:'{!! route('menuitemajax') !!}',
              method:'post',
              data:$('#menuform').serialize(),
@@ -144,6 +232,33 @@
                  {
                      window.location.reload(true);
                  }
+             }
+         });
+        });
+
+
+        $('.edit_item').click(function(e){
+            e.preventDefault();
+
+            var item = $(this).attr('data-id');
+            var csrf = "{!! csrf_token() !!}";
+         $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': csrf
+            }
+        });
+         $.ajax({
+             url:'{!! route('get-menuitem') !!}',
+             method:'post',
+             data:{id:item},
+             dataType: 'json',
+             success:function (response) {
+                $('#edit_text').val(response.text);
+                $('#edit_link_type option[value='+response.link_type+']').attr('selected','selected');
+                $('#edit_type option[value='+response.type+']').attr('selected','selected');
+                $('#edit_link option[value='+response.url+']').attr('selected','selected');
+                $('#edit_item_id').val(item);
+                $('#editinlineForm').modal('toggle');
              }
          });
         });
